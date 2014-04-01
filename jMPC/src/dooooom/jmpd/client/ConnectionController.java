eae697443c15dd8bbd5aa97c4dc1f68218a0aef5
Philip Rodning <philip.rodning@gmail.com>
2014-04-01 09:44:29 -0400

    initial commit connection controller

---
+++ jMPC/src/dooooom/jmpd/client/ConnectionController.java
@@ -0,0 +1,11 @@
+package dooooom.jmpd.client;
+
+/**
+ * This controller class is intended to store connection state information for the daemon,
+ * as well as handling all daemon calls themselves.  These daemon calls should be presented
+ * as public non-static methods that will act according to the state information stored
+ * in an instance of this class.
+ */
+public class ConnectionController {
+	
+}
