e4a999a2e739e04b8c346e541a40cd6487f3cce4
Philip Rodning <philip.rodning@gmail.com>
2014-04-09 13:18:22 -0400

    added tcp methods to connectioncontroller

--- jMPC/src/dooooom/jmpd/client/ConnectionController.java
+++ jMPC/src/dooooom/jmpd/client/ConnectionController.java
@@ -1,5 +1,12 @@
 package dooooom.jmpd.client;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+
 /**
  * This controller class is intended to store connection state information for the daemon,
  * as well as handling all daemon calls themselves.  These daemon calls should be presented
@@ -7,5 +14,27 @@ package dooooom.jmpd.client;
  * in an instance of this class.
  */
 public class ConnectionController {
+	private Socket socket;
 	
+	public ConnectionController(String host, int port) throws Exception {
+		socket = new Socket(InetAddress.getByName(host), port);
+	}
+	
+	/*
+	 * sendMsg itself is private.
+	 * all calls to daemon are abstracted into other methods.
+	 */
+	private String sendMsg(String msg) throws Exception {
+		String response = null;
+		BufferedReader is = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+		DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+		outToServer.writeBytes(msg);
+		
+		response = is.readLine(); 
+		return response;
+	}
+	
+	public void closeConnection() throws Exception {
+		socket.close();
+	}
 }
