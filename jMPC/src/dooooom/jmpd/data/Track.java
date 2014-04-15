c2d4991ce10f713aba46451fe970c8319c8fd812
ekakpo <ekakpo3@gmail.com>
2014-04-14 20:21:23 -0400

    Contructors added

--- jMPC/src/dooooom/jmpd/data/Track.java
+++ jMPC/src/dooooom/jmpd/data/Track.java
@@ -2,6 +2,17 @@ package dooooom.jmpd.data;
 
 import java.util.HashMap;
 
-public class Track extends HashMap<String,String> {
+public class Track extends HashMap<String,String> 
+{
+	public Track(String artist, String album, String title)
+	{
+		this.put("artist",artist);
+		this.put("album", album);
+		this.put("title", title);
+	}
 	
+	public Track()
+	{
+		this("Unknown Artist","Unknown Album","Unknown Title");
+	}
 }
